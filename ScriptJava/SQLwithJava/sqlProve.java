52ea142b7bf622e6a1c918738b120c447c1eb13d
fede_rake <federico.sgaramella12@gmail.com>
2023-03-03 11:22:46 +0100

    Update sqlProve.java

--- ScriptJava/SQLwithJava/sqlProve.java
+++ ScriptJava/SQLwithJava/sqlProve.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
+//import java.sql.Statement;
 
 public class sqlProve {
 
