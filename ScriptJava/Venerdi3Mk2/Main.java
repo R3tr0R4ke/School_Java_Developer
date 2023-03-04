2b9a9074b1685e1af9fb5ebc7fc13f4acf870fc1
fede_rake <federico.sgaramella12@gmail.com>
2023-03-04 18:35:32 +0100

    prova

---
+++ ScriptJava/Venerdi3Mk2/Main.java
@@ -0,0 +1,9 @@
+package School_Java_Developer.ScriptJava.Venerdi3Mk2;
+
+public class Main {
+    public static Utente utenteObj = new Utente(null, null);
+
+    public static void main(String[] args) {
+        MetodiUtente.menuRegLogin();
+    }
+}
